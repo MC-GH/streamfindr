1dbe3c2571dccdde623900fa78b52ecc1b07766f
Michaël <cornelismichael@hotmail.com>
2023-11-03 20:19:49 +0100

    First commit

---
+++ src/main/java/be/thomasmore/streamfindr/StreamfindrApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.streamfindr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamfindrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamfindrApplication.class, args);
+    }
+
+}
