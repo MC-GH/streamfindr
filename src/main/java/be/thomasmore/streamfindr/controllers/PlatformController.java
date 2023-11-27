3ccbe5800038ac704166bdccd740eaf188ae432a
Michaël <cornelismichael@hotmail.com>
2023-11-27 10:20:10 +0100

    Start wortking platformlist

--- src/main/java/be/thomasmore/streamfindr/controllers/PlatformController.java
+++ src/main/java/be/thomasmore/streamfindr/controllers/PlatformController.java
@@ -1,9 +1,13 @@
 package be.thomasmore.streamfindr.controllers;
 
+import be.thomasmore.streamfindr.model.Platform;
 import be.thomasmore.streamfindr.repositories.PlatformRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class PlatformController {
@@ -11,8 +15,10 @@ public class PlatformController {
     @Autowired
     private PlatformRepository platformRepository;
 
-    @GetMapping("/platformlist")
-    public String platformList() {
+    @GetMapping({"/platformlist", "/platformlist/"})
+    public String platformList(Model model) {
+        List<Platform> allPlatforms = platformRepository.findAll();
+        model.addAttribute("platforms", allPlatforms);
         return "platformlist";
     }
 
--- src/main/java/be/thomasmore/streamfindr/repositories/PlatformRepository.java
+++ src/main/java/be/thomasmore/streamfindr/repositories/PlatformRepository.java
@@ -3,5 +3,9 @@ package be.thomasmore.streamfindr.repositories;
 import be.thomasmore.streamfindr.model.Platform;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PlatformRepository extends CrudRepository<Platform,Integer> {
+
+    List<Platform> findAll();
 }
